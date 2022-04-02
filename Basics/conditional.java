af8e667ff95e75daaff344de63fca878a0dde389
amirahad <arahad4783@gmail.com>
2022-04-03 01:04:04 +0600

    calculator

---
+++ Basics/conditional.java
@@ -0,0 +1,49 @@
+package Basics;
+import java.util.*;
+
+public class conditional {
+    public static void main(String[] args) {
+        System.out.println("Choose operator '+','-','*'or '/'");
+        Scanner sc = new Scanner(System.in);
+        char operator = sc.next().charAt(0);
+        System.out.println("Enter the first value:");
+        int a = sc.nextInt();
+        System.out.println("Enter the second value:");
+        int b = sc.nextInt();
+        int c;
+        switch (operator){
+            case '+':
+                c = a+b;
+                System.out.println(c);
+                break;
+            case '-':
+                c = a-b;
+                System.out.println(c);
+                break;
+            case '*':
+                c = a*b;
+                System.out.println(c);
+                break;
+            case '/':
+                if(a<b){
+                    System.out.println("check your inputs, First input should be greater than second for division");
+                }else{
+                    c = a/b;
+                    System.out.println(c);
+                }
+                // float e = (float)c;
+                // e = a/b;
+                // System.out.println(e);
+                break;
+                
+            default:
+                System.out.println("error occoured");
+                break;
+        }
+
+        // if (age>18){
+        //     System.out.println("adult");
+        // }
+        // System.out.println("not adult");
+    }
+}
